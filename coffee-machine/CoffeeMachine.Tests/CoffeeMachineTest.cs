9a6dfd117832ca1903900a4ab6b51d992ac2618c
Lorenzo <llanuza@aidacanarias.com>
2024-04-12 09:44:52 +0100

    test: order_coffee

--- coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
+++ coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
@@ -1,3 +1,4 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace CoffeeMachine.Tests
@@ -5,9 +6,60 @@ namespace CoffeeMachine.Tests
     public class CoffeeMachineTest
     {
         [Test]
-        public void Fix_Me_And_Rename_Me()
+        public void order_coffee()
         {
-            Assert.That(false, Is.True);
+            //Given
+            var driver = Substitute.For<Driver>();
+            var coffeeMachine = new CoffeeMachineApp(driver);
+            var expectedOrder = new Order()
+            {
+                Drink = Drink.Coffee
+            };
+
+            //When
+            coffeeMachine.SelectCoffee();
+            coffeeMachine.MakeDrink();
+
+            //Then
+            driver.Received(1).SendOrder(Arg.Is<Order>(o => o.Drink == expectedOrder.Drink));
         }
+    }
+
+    public class CoffeeMachineApp
+    {
+        private readonly Driver _driver;
+
+        public CoffeeMachineApp(Driver driver)
+        {
+            _driver = driver;
+        }
+
+        public void SelectCoffee()
+        {
+            
+        }
+
+        public void MakeDrink()
+        {
+            _driver.SendOrder(new Order()
+            {
+                Drink = Drink.Coffee
+            });
+        }
+    }
+
+    public interface Driver
+    {
+        void SendOrder(Order order);
+    }
+
+    public class Order
+    {
+        public Drink Drink { get; set; }
+    }
+
+    public enum Drink
+    {
+        Coffee
     }
 }
