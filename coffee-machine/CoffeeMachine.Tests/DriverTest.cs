556cfc00e7d1721e3bfc910cc76e12210cffa59e
Lorenzo <llanuza@aidacanarias.com>
2024-04-12 10:46:30 +0100

    test: transform_chocolate_with_one_spoon_of_sugar_into_command

--- coffee-machine/CoffeeMachine.Tests/DriverTest.cs
+++ coffee-machine/CoffeeMachine.Tests/DriverTest.cs
@@ -35,6 +35,22 @@ public class DriverTest
 
         drinkMaker.Received(1).Execute("T::");
     }
+
+    [Test]
+    public void transform_chocolate_with_one_spoon_of_sugar_into_command()
+    {
+        var drinkMaker = Substitute.For<DrinkMaker>();
+        var givenOrder = new Order()
+        {
+            Drink = Drink.Chocolate,
+            SpoonOfSugar = 1
+        };
+        var driver = new Driver800(drinkMaker);
+
+        driver.SendOrder(givenOrder);
+
+        drinkMaker.Received(1).Execute("H:1:0");
+    }
 }
 
 public class Driver800 : Driver
@@ -48,7 +64,9 @@ public class Driver800 : Driver
 
     public void SendOrder(Order order)
     {
-        _drinkMaker.Execute($"{TranslateDrinkTypeToCommandFormat(order.Drink)}::");
+        var spoonsOfSugar = order.SpoonOfSugar > 0 ? order.SpoonOfSugar.ToString() : string.Empty;
+        var useStick = order.UseStick ? "0" : string.Empty;
+        _drinkMaker.Execute($"{TranslateDrinkTypeToCommandFormat(order.Drink)}:{spoonsOfSugar}:{useStick}");
     }
 
     private string TranslateDrinkTypeToCommandFormat(Drink drink) =>
@@ -56,6 +74,7 @@ public class Driver800 : Driver
         {
             Drink.Coffee => "C",
             Drink.Tea => "T",
+            Drink.Chocolate => "H",
             _ => throw new NotImplementedException()
         };
 }
