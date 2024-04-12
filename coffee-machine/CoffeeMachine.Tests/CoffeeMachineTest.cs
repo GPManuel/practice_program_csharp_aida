c6db11a6fc30b211131737e7fb8d93f73ac0697f
Lorenzo <llanuza@aidacanarias.com>
2024-04-12 10:07:30 +0100

    test: order_tea

--- coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
+++ coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
@@ -23,11 +23,30 @@ namespace CoffeeMachine.Tests
             //Then
             driver.Received(1).SendOrder(Arg.Is<Order>(o => o.Drink == expectedOrder.Drink));
         }
+
+        [Test]
+        public void order_tea()
+        {
+            var driver = Substitute.For<Driver>();
+            var coffeeMachine = new CoffeeMachineApp(driver);
+            var expectedOrder = new Order()
+            {
+                Drink = Drink.Tea
+            };
+
+            coffeeMachine.SelectTea();
+            coffeeMachine.MakeDrink();
+
+            driver.Received(1).SendOrder(Arg.Is<Order>(o => o.Drink == expectedOrder.Drink));
+        }
     }
 
     public class CoffeeMachineApp
     {
         private readonly Driver _driver;
+
+        private Drink _currentDrink;
+
 
         public CoffeeMachineApp(Driver driver)
         {
@@ -36,14 +55,19 @@ namespace CoffeeMachine.Tests
 
         public void SelectCoffee()
         {
-            
+            _currentDrink = Drink.Coffee;
+        }
+
+        public void SelectTea()
+        {
+            _currentDrink = Drink.Tea;
         }
 
         public void MakeDrink()
         {
             _driver.SendOrder(new Order()
             {
-                Drink = Drink.Coffee
+                Drink = _currentDrink
             });
         }
     }
@@ -60,6 +84,7 @@ namespace CoffeeMachine.Tests
 
     public enum Drink
     {
-        Coffee
+        Coffee,
+        Tea
     }
 }
