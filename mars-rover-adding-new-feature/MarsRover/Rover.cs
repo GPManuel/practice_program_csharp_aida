c993fb70d1fed93550d50540b3290ea0d475e627
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-05-02 11:02:19 +0200

    ♻️ remove data clumb

--- mars-rover-adding-new-feature/MarsRover/Rover.cs
+++ mars-rover-adding-new-feature/MarsRover/Rover.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace MarsRover
@@ -7,8 +6,7 @@ namespace MarsRover
     public class Rover
     {
         private const int Displacement = 1;
-        private Direction _direction;
-        private Coordinates _coordinates;
+        private Location _location;
 
         public Rover(int x, int y, string direction)
         {
@@ -19,8 +17,7 @@ namespace MarsRover
 
         private void SetLocation(Direction direction, Coordinates coordinates)
         {
-            _direction = direction;
-            _coordinates = coordinates;
+            _location = new Location(direction, coordinates);
         }
 
         public void Receive(string commandsSequence)
@@ -30,20 +27,20 @@ namespace MarsRover
             {
                 if (command.Equals("l"))
                 {
-                    SetLocation(_direction.RotateLeft(), _coordinates);
+                    SetLocation(_location.GetDirection().RotateLeft(), _location.GetCoordinates());
                 }
                 else if (command.Equals("r"))
                 {
-                    SetLocation(_direction.RotateRight(), _coordinates);
+                    SetLocation(_location.GetDirection().RotateRight(), _location.GetCoordinates());
 
                 }
                 else if (command.Equals("f"))
                 {
-                    SetLocation(_direction, _direction.Move(_coordinates, Displacement));
+                    SetLocation(_location.GetDirection(), _location.GetDirection().Move(_location.GetCoordinates(), Displacement));
                 }
                 else
                 {
-                    SetLocation(_direction, _direction.Move(_coordinates, -Displacement));
+                    SetLocation(_location.GetDirection(), _location.GetDirection().Move(_location.GetCoordinates(), -Displacement));
                 }
             }
         }
@@ -52,13 +49,48 @@ namespace MarsRover
         {
             if (ReferenceEquals(null, obj)) return false;
             if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
+            if (obj.GetType() != this.GetType()) return false;
             return Equals((Rover)obj);
         }
 
         protected bool Equals(Rover other)
         {
+            return Equals(_location, other._location);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_location != null ? _location.GetHashCode() : 0);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(_location)}: {_location}";
+        }
+    }
+
+    internal class Location
+    {
+        private readonly Direction _direction;
+        private readonly Coordinates _coordinates;
+
+        public Location(Direction direction, Coordinates coordinates)
+        {
+            _direction = direction;
+            _coordinates = coordinates;
+        }
+
+        protected bool Equals(Location other)
+        {
             return Equals(_direction, other._direction) && Equals(_coordinates, other._coordinates);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Location)obj);
         }
 
         public override int GetHashCode()
@@ -69,6 +101,16 @@ namespace MarsRover
         public override string ToString()
         {
             return $"{nameof(_direction)}: {_direction}, {nameof(_coordinates)}: {_coordinates}";
+        }
+
+        public Direction GetDirection()
+        {
+            return _direction;
+        }
+
+        public Coordinates GetCoordinates()
+        {
+            return _coordinates;
         }
     }
 }
