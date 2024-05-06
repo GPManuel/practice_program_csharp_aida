020fb64c91f37c404fa8f5ae278b9773dcaa441d
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-05-06 15:16:17 +0100

    eliminar duplicación uso constructor rover

--- mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverRotationTests.cs
+++ mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverRotationTests.cs
@@ -1,86 +1,86 @@
 using NUnit.Framework;
+using static MarsRover.Tests.RoverBuilder;
 
 namespace MarsRover.Tests;
 
 public class RoverRotationTests
 {
     [Test]
-    public void Facing_North_Rotate_Left()
-    {
-        var rover = new Rover(0, 0, "N");
+    public void Facing_North_Rotate_Left() {
+        var rover = ANASARover().Facing("N").Build();
 
         rover.Receive("l");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "W")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("W").Build()));
     }
 
     [Test]
     public void Facing_North_Rotate_Right()
     {
-        var rover = new Rover(0, 0, "N");
+        var rover = ANASARover().Facing("N").Build();
 
         rover.Receive("r");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "E")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("E").Build()));
     }
 
     [Test]
     public void Facing_South_Rotate_Left()
     {
-        var rover = new Rover(0, 0, "S");
+        var rover = ANASARover().Facing("S").Build();
 
         rover.Receive("l");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "E")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("E").Build()));
     }
 
     [Test]
     public void Facing_South_Rotate_Right()
     {
-        var rover = new Rover(0, 0, "S");
+        var rover = ANASARover().Facing("S").Build();
 
         rover.Receive("r");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "W")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("W").Build()));
     }
 
     [Test]
     public void Facing_West_Rotate_Left()
     {
-        var rover = new Rover(0, 0, "W");
+        var rover = ANASARover().Facing("W").Build();
 
         rover.Receive("l");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "S")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("S").Build()));
     }
 
     [Test]
     public void Facing_West_Rotate_Right()
     {
-        var rover = new Rover(0, 0, "W");
+        var rover = ANASARover().Facing("W").Build();
 
         rover.Receive("r");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "N")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("N").Build()));
     }
 
     [Test]
     public void Facing_East_Rotate_Left()
     {
-        var rover = new Rover(0, 0, "E");
+        var rover = ANASARover().Facing("E").Build();
 
         rover.Receive("l");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "N")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("N").Build()));
     }
 
     [Test]
     public void Facing_East_Rotate_Right()
     {
-        var rover = new Rover(0, 0, "E");
+        var rover = ANASARover().Facing("E").Build();
 
         rover.Receive("r");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "S")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Facing("S").Build()));
     }
 }
