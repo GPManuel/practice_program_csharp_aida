f6d6d93997defc8ffda3eeda462caf0364ecce91
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-05-06 15:11:48 +0100

    eliminar duplicación uso constructor rover

--- mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverReceivingCommandsListTests.cs
+++ mars-rover-adding-new-feature-cont/MarsRover.Tests/RoverReceivingCommandsListTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using static MarsRover.Tests.RoverBuilder;
 
 namespace MarsRover.Tests;
 
@@ -7,30 +8,30 @@ public class RoverReceivingCommandsListTests
     [Test]
     public void No_Commands()
     {
-        var rover = new Rover(0, 0, "N");
+        var rover = ANASARover().Build();
 
         rover.Receive("");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "N")));
+        Assert.That(rover, Is.EqualTo(ANASARover().Build()));
     }
 
     [Test]
     public void Two_Commands()
     {
-        var rover = new Rover(0, 0, "N");
+        var rover = ANASARover().WithCoordinates(0,0).Facing("N").Build(); 
 
         rover.Receive("lf");
 
-        Assert.That(rover, Is.EqualTo(new Rover(-1, 0, "W")));
+        Assert.That(rover, Is.EqualTo(ANASARover().WithCoordinates(-1, 0).Facing("W").Build()));
     }
 
     [Test]
     public void Many_Commands()
     {
-        var rover = new Rover(0, 0, "N");
+        var rover = ANASARover().WithCoordinates(0, 0).Facing("N").Build();
 
         rover.Receive("ffrbbrfflff");
 
-        Assert.That(rover, Is.EqualTo(new Rover(0, 0, "E")));
+        Assert.That(rover, Is.EqualTo(ANASARover().WithCoordinates(0, 0).Facing("E").Build()));
     }
 }
