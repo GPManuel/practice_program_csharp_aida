eeab978e17d9491625715347f6f189985ac0c826
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-06-10 13:35:49 +0200

    ✅ move_character_to_right_when_shift_is_positive

--- legacy-security-manager-cont-2/LegacySecurityManager.Tests/CaesarPasswordEncryptorTest.cs
+++ legacy-security-manager-cont-2/LegacySecurityManager.Tests/CaesarPasswordEncryptorTest.cs
@@ -1,23 +1,28 @@
 ﻿using NUnit.Framework;
 
-namespace LegacySecurityManager.Tests
+namespace LegacySecurityManager.Tests;
+
+public class CaesarPasswordEncryptorTest
 {
-    /*
-     *ABC -->(0) ABC
-       ABC -->(1) BCD
-       ABC -->(-1) ZAB
-     */
-    public class CaesarPasswordEncryptorTest
+    [Test]
+    public void password_not_change_when_shift_is_0()
     {
-        [Test]
-        public void password_not_change_when_shift_is_0()
-        {
-            var password = "ABC";
-            var caesarPasswordEncrypter = new CaesarPasswordEncryptor(0);
+        var password = "ABC";
+        var caesarPasswordEncryptor = new CaesarPasswordEncryptor(0);
 
-            var encryptedPassword = caesarPasswordEncrypter.Encrypt(password);
+        var encryptedPassword = caesarPasswordEncryptor.Encrypt(password);
 
-            Assert.That(encryptedPassword, Is.EqualTo(password));
-        }
+        Assert.That(encryptedPassword, Is.EqualTo(password));
+    }
+
+    [Test]
+    public void move_character_to_right_when_shift_is_positive()
+    {
+        var password = "ABC";
+        var caesarPasswordEncryptor = new CaesarPasswordEncryptor(2);
+
+        var encryptedPassword = caesarPasswordEncryptor.Encrypt(password);
+
+        Assert.That(encryptedPassword, Is.EqualTo("CDE"));
     }
 }
--- legacy-security-manager-cont-2/LegacySecurityManager/CaesarPasswordEncryptor.cs
+++ legacy-security-manager-cont-2/LegacySecurityManager/CaesarPasswordEncryptor.cs
@@ -1,8 +1,11 @@
-﻿namespace LegacySecurityManager;
+﻿using System.Linq;
+
+namespace LegacySecurityManager;
 
 public class CaesarPasswordEncryptor : PasswordEncryptor
 {
     private readonly int _shift;
+    private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public CaesarPasswordEncryptor(int shift)
     {
@@ -11,6 +14,19 @@ public class CaesarPasswordEncryptor : PasswordEncryptor
 
     public string Encrypt(string password)
     {
+        if (_shift > 0)
+        {
+            return new string(password.Select(character => Transform(character)).ToArray());
+        }
         return password;
+    }
+
+    private char Transform(char character)
+    {
+        var passwordTranformed = 'a';
+        var index = alphabet.IndexOf(character);
+        passwordTranformed = alphabet[_shift + index];
+
+        return passwordTranformed;
     }
 }
