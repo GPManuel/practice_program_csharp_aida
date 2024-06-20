1046f320dc1a20507ae06508538c104731e77d48
Manuel Gallud Peñalver <mgallud@aidacanarias.com>
2024-06-20 11:39:28 +0200

    ✅ player_2_win_4_points_and_win_the_match

--- tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
+++ tennis-game-score-board/Tennis.Tests/GameScoreBoardTest.cs
@@ -60,4 +60,20 @@ public class GameScoreBoardTest
         display.Received(1).Show("Forty Love");
         display.Received(1).Show("Player 1 has won!!\nIt was a nice game.\nBye now");
     }
+
+    [Test]
+    public void player_2_win_4_points_and_win_the_match()
+    {
+        var refereeInput = Substitute.For<RefereeInput>();
+        var display = Substitute.For<Display>();
+        var gameScoreBoard = new GameScoreBoard(refereeInput, display);
+        refereeInput.GetScore().Returns("score 2", "score 2", "score 2", "score 2");
+
+        gameScoreBoard.StartGame();
+
+        display.Received(1).Show("Love Fifteen");
+        display.Received(1).Show("Love Thirty");
+        display.Received(1).Show("Love Forty");
+        display.Received(1).Show("Player 2 has won!!\nIt was a nice game.\nBye now");
+    }
 }
--- tennis-game-score-board/Tennis/GameScoreBoard.cs
+++ tennis-game-score-board/Tennis/GameScoreBoard.cs
@@ -5,12 +5,14 @@ public class GameScoreBoard
     private readonly RefereeInput _refereeInput;
     private readonly Display _display;
     private int _playerOneScore;
+    private int _playerTwoScore;
 
     public GameScoreBoard(RefereeInput refereeInput, Display display)
     {
         _refereeInput = refereeInput;
         _display = display;
         _playerOneScore = 0;
+        _playerTwoScore = 0;
     }
 
     public void StartGame() {
@@ -21,21 +23,44 @@ public class GameScoreBoard
         Score();
         _display.Show($"{GetPlayerOneScoreDisplay()} {GetPlayerTwoScoreDisplay()}");
         Score();
-        _display.Show("Player 1 has won!!\nIt was a nice game.\nBye now");
+        var winner = GetWinner();
+        _display.Show($"Player {winner} has won!!\nIt was a nice game.\nBye now");
     }
 
-    private string GetPlayerTwoScoreDisplay() {
+    private string GetWinner()
+    {
+        if (_playerOneScore > _playerTwoScore)
+        {
+            return "1";
+        }
+
+        return "2";
+    }
+
+    private string GetPlayerTwoScoreDisplay()
+    {
+        if (_playerTwoScore == 3) return "Forty";
+        if (_playerTwoScore == 2) return "Thirty";
+        if (_playerTwoScore == 1) return "Fifteen";
         return "Love";
     }
 
     private string GetPlayerOneScoreDisplay() {
         if (_playerOneScore == 3) return "Forty";
         if (_playerOneScore == 2) return "Thirty";
-        return "Fifteen";
+        if (_playerOneScore == 1) return "Fifteen";
+        return "Love";
     }
 
     private void Score() {
         var score = _refereeInput.GetScore();
-        _playerOneScore++;
+        if (score.Equals("score 1"))
+        {
+            _playerOneScore++;
+        }
+        else
+        {
+            _playerTwoScore++;
+        }
     }
 }
