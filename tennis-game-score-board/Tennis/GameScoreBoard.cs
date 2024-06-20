9e9327317737c19fb09ba4324bd620266ad6369d
Kilian Suárez <ksuarez@domingoalonsogroup.com>
2024-06-20 10:26:46 +0100

    refactor

--- tennis-game-score-board/Tennis/GameScoreBoard.cs
+++ tennis-game-score-board/Tennis/GameScoreBoard.cs
@@ -4,18 +4,38 @@ public class GameScoreBoard
 {
     private readonly RefereeInput _refereeInput;
     private readonly Display _display;
+    private int _playerOneScore;
 
     public GameScoreBoard(RefereeInput refereeInput, Display display)
     {
         _refereeInput = refereeInput;
         _display = display;
+        _playerOneScore = 0;
     }
 
-    public void StartGame()
-    {
-        _display.Show("Fifteen Love");
-        _display.Show("Thirty Love");
-        _display.Show("Forty Love");
+    public void StartGame() {
+        Score();
+        _display.Show($"{GetPlayerOneScoreDisplay()} {GetPlayerTwoScoreDisplay()}");
+        Score();
+        _display.Show($"{GetPlayerOneScoreDisplay()} {GetPlayerTwoScoreDisplay()}");
+        Score();
+        _display.Show($"{GetPlayerOneScoreDisplay()} {GetPlayerTwoScoreDisplay()}");
+        Score();
         _display.Show("Player 1 has won!!\nIt was a nice game.\nBye now");
+    }
+
+    private string GetPlayerTwoScoreDisplay() {
+        return "Love";
+    }
+
+    private string GetPlayerOneScoreDisplay() {
+        if (_playerOneScore == 3) return "Forty";
+        if (_playerOneScore == 2) return "Thirty";
+        return "Fifteen";
+    }
+
+    private void Score() {
+        var score = _refereeInput.GetScore();
+        _playerOneScore++;
     }
 }
